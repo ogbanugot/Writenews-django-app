f67eef951632c43a7f690a99c2870557df7440df
Ogban-Asuquo Ugot <ogbanugot@gmail.com>
2018-09-18 06:27:30 +0100

    Update settings.py

--- WriteNews/settings.py
+++ WriteNews/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
